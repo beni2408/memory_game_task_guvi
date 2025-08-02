052960a605198aa448294d1a35ada4664d038fd7
beni2408 <jascarbenish@gmail.com>
2025-08-02 14:47:25 +0530

    implemented the button logic

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function shuffle(val1, val2, val3, val4, val5, val6) {
   const shuffledvalues = [val1, val2, val3, val4, val5, val6].sort(
     () => Math.random() - 0.5
   );
-  console.log(shuffledvalues);
+  return shuffledvalues;
 }
 
-shuffle(1, 2, 3, 4, 5, 6);
+const val = shuffle("❤️", "🎶", "✨", "🌟", "🎈", "🔥");
+
+console.log(val);
+
+// code
+
+const refreshBTN = document.getElementById("restart-btn");
+refreshBTN.addEventListener("click", () => {
+  const newShuffle = shuffle("❤️", "🎶", "✨", "🌟", "🎈", "🔥");
+  console.log(newShuffle);
+});
