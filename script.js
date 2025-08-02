e7c7bed478a0970f3b283bcbd464b63c1b5dc6da
beni2408 <jascarbenish@gmail.com>
2025-08-02 15:19:52 +0530

    worked on the shuffling with button

--- script.js
+++ script.js
@@ -13,6 +13,30 @@ console.log(val);
 
 const refreshBTN = document.getElementById("restart-btn");
 refreshBTN.addEventListener("click", () => {
-  const newShuffle = shuffle("❤️", "🎶", "✨", "🌟", "🎈", "🔥");
-  console.log(newShuffle);
+  //   const newShuffle = shuffle("❤️", "🎶", "✨", "🌟", "🎈", "🔥");
+  //   console.log(newShuffle);
+  const [a, b, c, d, e, f] = shuffle("❤️", "🎶", "✨", "🌟", "🎈", "🔥");
+  console.log(a, b, c, d, e, f);
+  const emoji41 = document.getElementById("emoji41");
+  const emoji42 = document.getElementById("emoji42");
+  const emoji43 = document.getElementById("emoji43");
+  const emoji21 = document.getElementById("emoji21");
+  const emoji22 = document.getElementById("emoji22");
+  const emoji23 = document.getElementById("emoji23");
+  const emoji31 = document.getElementById("emoji31");
+  const emoji32 = document.getElementById("emoji32");
+  const emoji33 = document.getElementById("emoji33");
+
+  emoji41.textContent = a;
+  emoji42.textContent = e;
+  emoji43.textContent = c;
+  emoji33.textContent = a;
+  emoji32.textContent = f;
+  emoji31.textContent = e;
+  emoji21.textContent = b;
+  emoji22.textContent = f;
+  emoji23.textContent = b;
+  emoji13.textContent = c;
+  emoji11.textContent = d;
+  emoji12.textContent = d;
 });
